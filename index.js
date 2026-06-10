acc13399226e3d05b3e4dfefa8db347e9a8e63fc
MiikkaVaa <miikka.vaananen@helsinki.fi>
2026-06-10 18:05:45 +0300

    testing if fix worked

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express-app</h1>')
+  res.send('<h1>pls work</h1>')
 })
 
 const PORT = 8080
